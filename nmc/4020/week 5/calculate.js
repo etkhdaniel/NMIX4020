6fbbff56b9bb8de9ca2054e9922bd81c6fc0f984
Eva Daniel <61015593+etkhdaniel@users.noreply.github.com>
2023-02-01 09:08:30 -0500

    -

--- nmc/4020/week 5/calculate.js
+++ nmc/4020/week 5/calculate.js
@@ -1,7 +1,5 @@
 let input1 = document.querySelector('#input1');
 let input2 = document.querySelector('#input2');
-let input3 = document.querySelector('#input3');
-let input4 = document.querySelector('#input4');
 let additionButton = document.querySelector('#addButton');
 let subtractionButton = document.querySelector('#subButton');
 let outputField = document.querySelector('#output');
@@ -18,12 +16,34 @@ additionButton.onclick = function() {
 }
 
 function sub() {
-    let subtraction1 = Number(input3.value);
-    let subtraction2 = Number(input4.value);
+    let subtraction1 = Number(input1.value);
+    let subtraction2 = Number(input2.value);
     let difference = subtraction1 + subtraction2;
     outputField.value = difference;
 }
 
-subtractionButton.onclick = function () {
+subtractionButton.onclick = function() {
     sub();
+}
+
+function div() {
+    let division1 = Number(input1.value);
+    let division2 = Number(input2.value);
+    let quotient = division1 / division2;
+    outputField.value = quotient;
+}
+
+divisionButton.onclick = function() {
+    div();
+}
+
+function times() {
+    let multiply1 = Number(input1.value);
+    let multiply2 = Number(input2.value);
+    let product = multiply1 * multiply2;
+    outputField.value = product;
+}
+
+timesButton.onclick = function() {
+    times();
 }
