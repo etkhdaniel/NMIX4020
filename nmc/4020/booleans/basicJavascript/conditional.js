6403db285835d255bac29e266193a53f3f84f678
Eva Daniel <61015593+etkhdaniel@users.noreply.github.com>
2023-02-13 10:22:03 -0500

    -

--- nmc/4020/booleans/basicJavascript/conditional.js
+++ nmc/4020/booleans/basicJavascript/conditional.js
@@ -1,8 +1,27 @@
-let answer = 24;
+let answer = document.querySelector('#userInput');
+answer = Number(answer); 
 let reaction = document.querySelector('#response')
 
-if (answer == 24) {
-    reaction.innerHTML = "correct";
+/** if (answer < 24 && answer > 20) {
+    reaction.innerHTML = "The answer is too low but close";
+} else if (answer < 19) {
+    reaction.innerHTML = "The answer is way to low"
+} else if (answer > 24 && answer < 28) {
+    reaction.innerHTML = "The answer is too high but close"
+} else if (answer > 29) {
+    reaction.innerHTML = "The answer is way too high";
 } else {
-    reaction.innerHTML="wrong answer";
+    reaction.innerHTML = "The answer is correct"
+} */
+
+switch(true) {
+    case (answer == 24):
+        reaction.innerHTML = "correct";
+        break;
+    case (answer < 24):
+        reaction.innerHTML = "too low";
+        break;
+    case (answer > 24 && answer < 28):
+        reaction.innerHTML = "too high";
+        break;
 }
