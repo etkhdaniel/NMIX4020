de14952a57a906a58e7aefa3ac30f9fc3ea46b8a
Eva Daniel <61015593+etkhdaniel@users.noreply.github.com>
2023-04-03 09:22:47 -0400

    -

---
+++ nmc/4020/week 11/intro to vue/assets/js/newVue.js
@@ -0,0 +1,12 @@
+var header = new Vue ({
+    el: '#header',
+    data: {
+        yourName: 'Eva Daniel',
+        description: "I am a CS major",
+        linkOne: "Introduction",
+        link2: "My Portfolio",
+        link3: "About Me, OK??",
+        link4: "Contact",
+        link5: "my list"
+    }
+})
