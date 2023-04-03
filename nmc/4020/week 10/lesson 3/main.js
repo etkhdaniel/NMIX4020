e674745cf116f53fc90473a90adc3e17c971e161
Eva Daniel <61015593+etkhdaniel@users.noreply.github.com>
2023-04-03 09:37:53 -0400

    -

--- nmc/4020/week 10/lesson 3/main.js
+++ nmc/4020/week 10/lesson 3/main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            image: 'images/socks_green.jpg',
             // solution
             url: 'https://www.vuemastery.com/'
             // solution
