ea06c1ff9fbe12c676cfbb3764020c3b2f26e1a8
Eva Daniel <61015593+etkhdaniel@users.noreply.github.com>
2023-05-11 23:19:47 -0400

    -

--- nmc/4020/week 15/countryguide/script.js
+++ nmc/4020/week 15/countryguide/script.js
@@ -1,23 +1,31 @@
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
+let cvalue = "USD";
+let earl = " <a href='currency/currency.html'> see value </a>";
+
+
 searchBtn.addEventListener("click", () => {
-  let countryName = countryInp.value;
-  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-  console.log(finalURL);
-  fetch(finalURL)
-    .then((response) => response.json())
-    .then((data) => {
-      //   console.log(data[0]);
-      //   console.log(data[0].capital[0]);
-      //   console.log(data[0].flags.svg);
-      //   console.log(data[0].name.common);
-      //   console.log(data[0].continents[0]);
-      //   console.log(Object.keys(data[0].currencies)[0]);
-      //   console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-      //   console.log(
-      //     Object.values(data[0].languages).toString().split(",").join(", ")
-      //   );
-      result.innerHTML = `
+    let countryName = countryInp.value;
+
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    console.log(finalURL);
+    fetch(finalURL)
+        .then((response) => response.json())
+        .then((data) => {
+            //   console.log(data[0]);
+            //   console.log(data[0].capital[0]);
+            //   console.log(data[0].flags.svg);
+            //   console.log(data[0].name.common);
+            //   console.log(data[0].continents[0]);
+
+            cvalue = (Object.keys(data[0].currencies)[0]);
+            console.log(cvalue);
+            document.querySelector('#theurl').innerHTML = " <a href='currency/currency.html?cvalue=" + cvalue + "'> Convert Currency </a>";
+            // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+            //   console.log(
+            //     Object.values(data[0].languages).toString().split(",").join(", ")
+            //   );
+            result.innerHTML = `
         <img src="${data[0].flags.svg}" class="flag-img">
         <h2>${data[0].name.common}</h2>
         <div class="wrapper">
@@ -56,12 +64,12 @@ searchBtn.addEventListener("click", () => {
             </div>
         </div>
       `;
-    })
-    .catch(() => {
-      if (countryName.length == 0) {
-        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
-      } else {
-        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
-      }
-    });
+        })
+        .catch(() => {
+            if (countryName.length == 0) {
+                result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+            } else {
+                result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+            }
+        });
 });
